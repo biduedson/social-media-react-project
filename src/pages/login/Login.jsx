95b2ab737fd4ebd26e247a715df8a1e4959ea828
biduedson <112134265+biduedson@users.noreply.github.com>
2023-02-28 02:13:56 -0300

    arquivos react

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./login.scss"
+
+function Login() {
+    return (
+        <div className="login">
+            <div className="card">
+                <div className="left">
+                    <h1>Hello World</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius dolor tempore animi consequuntur ab.
+                        Tempora sequi aut debitis, hic accusantium iure, cupiditate facilis, aperiam officia vitae laborum?
+                        Unde, voluptate aliquam.
+                    </p>
+                    <span>Don't you have an acount?</span>
+                    <button>Register</button>
+                </div>
+                <div className="rigth">
+                    <h1>Login</h1>
+                    <form>
+                        <input type="text" placeholder='Username' />
+                        <input type="password" placeholder='Password' />
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./register.scss"
+
+function Register() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Register
